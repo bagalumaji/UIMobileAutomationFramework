baf7c26b2d439924835edcf52c1d59ebdf5f2062
bagalumaji <bagalumaji@gmail.com>
2022-07-13 00:32:14 +0530

    updated ChromeManager, driver

--- src/main/java/com/bagal/driver/Driver.java
+++ src/main/java/com/bagal/driver/Driver.java
@@ -1,10 +1,13 @@
 package com.bagal.driver;
 
+import com.bagal.configs.ConfigFactory;
+import com.bagal.driver.web.local.LocalDriverFactory;
+
 public final class Driver {
     private Driver(){}
 
     public static void intiDriver(){
-        LocalDriverFactory.getDriver();
+        LocalDriverFactory.getDriver(ConfigFactory.getConfig().browser());
     }
     public static void quiteDriver(){
 
--- src/main/java/com/bagal/driver/manager/web/local/ChromeManager.java
+++ src/main/java/com/bagal/driver/manager/web/local/ChromeManager.java
@@ -1,4 +1,4 @@
-package com.bagal.driver.manager;
+package com.bagal.driver.manager.web.local;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
