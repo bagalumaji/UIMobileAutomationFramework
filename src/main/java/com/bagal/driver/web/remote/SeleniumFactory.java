d1fe4a60c38d21e425a6ce06f07b3c4f12677503
bagalumaji <bagalumaji@gmail.com>
2022-07-13 00:29:02 +0530

    added Selenoid Factory

--- src/main/java/com/bagal/driver/web/remote/SeleniumFactory.java
+++ src/main/java/com/bagal/driver/web/remote/SeleniumFactory.java
@@ -5,8 +5,8 @@ import com.bagal.driver.manager.web.remote.selenium.SeleniumGridSafariManager;
 import com.bagal.enums.BrowserType;
 import org.openqa.selenium.WebDriver;
 
-public final class SeleniumGridFactory {
-    private SeleniumGridFactory(){}
+public final class SeleniumFactory {
+    private SeleniumFactory(){}
 
     public static WebDriver getDriver(BrowserType browserType){
         return browserType==BrowserType.CHROME
