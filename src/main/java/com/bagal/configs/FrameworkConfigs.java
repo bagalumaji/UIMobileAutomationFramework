4f9d8022ffb46774101f014386b71b943b4050d3
bagalumaji <bagalumaji@gmail.com>
2022-07-13 00:30:00 +0530

    added configs

--- src/main/java/com/bagal/configs/FrameworkConfigs.java
+++ src/main/java/com/bagal/configs/FrameworkConfigs.java
@@ -3,10 +3,13 @@ package com.bagal.configs;
 import com.bagal.convertors.StringToBrowserRemoteModeTypeConvertor;
 import com.bagal.convertors.StringToBrowserRunModeTypeConvertor;
 import com.bagal.convertors.StringToBrowserTypeConvertor;
+import com.bagal.convertors.StringToUrlConvertor;
 import com.bagal.enums.BrowserRemoteModeType;
 import com.bagal.enums.BrowserType;
 import com.bagal.enums.BrowserRunModeType;
 import org.aeonbits.owner.Config;
+
+import java.net.URL;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
@@ -26,4 +29,10 @@ public interface FrameworkConfigs extends Config {
 
    @ConverterClass(StringToBrowserRemoteModeTypeConvertor.class)
    BrowserRemoteModeType browserRemoteMode();
+
+   @ConverterClass(StringToUrlConvertor.class)
+   URL seleniumGridUrl();
+
+   @ConverterClass(StringToUrlConvertor.class)
+   URL selenoidGridUrl();
 }
--- src/test/java/com/bagal/tests/DemoTest.java
+++ src/test/java/com/bagal/tests/DemoTest.java
@@ -1,11 +1,11 @@
 package com.bagal.tests;
 
-import com.bagal.configs.FrameworkConfigFactory;
+import com.bagal.configs.ConfigFactory;
 import org.testng.annotations.Test;
 
 public class DemoTest {
     @Test
     public void demTest(){
-        System.out.println("config.browser() = " + FrameworkConfigFactory.getConfig().browser());
+        System.out.println("config.browser() = " + ConfigFactory.getConfig().browser());
     }
 }
